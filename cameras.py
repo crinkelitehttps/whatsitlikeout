1114d8c2023f74783bfb8c424374c8d18aa49a50
crinkelitehttps <crinkelite@yahoo.co.uk>
2016-12-07 18:51:23 +0000

    added execuatable names based on architechture

--- cameras.py
+++ cameras.py
@@ -7,22 +7,32 @@ import re
 urls = []
 
 def urlcheck():
+    global urls
     s = requests.Session()
     s.headers.update({"User-Agent":"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"})
     r = s.get("http://thecladdagh.com/webcam")
-    global urls
     for i in re.findall('rtmp:[\.\/:A-Za-z0-9]{,200}', str(r.content)):
+        print(i)
         urls.append(i)
 
-def playstream():
-    if os.system("uname -o") == "armv61":
+def pickplayer():
+    if os.system("uname -m") == "armv61":
         print("is arm")
+        return "omxplayer "
     else:
         print("not arm")
-        os.system("mplayer " +  str(urls[0]))
- 
+        return "mplayer " 
+
+def play(command, stream):
+    os.system(command + str(urls[stream]))
+
 if not urls:
     urlcheck()
-    playstream()
 else:
-    playstream()
+    pass
+
+#savedUrl = str(open("$USER/.config/whatsitlikeout").read())
+print('Press 0 for shop street\nPress 1 for High\ Street')
+stream = int(input())
+command = pickplayer()
+play(command, stream)
